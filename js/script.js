718182eaf165cc5a59a5ccf2a30fc136b21bac8c
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-12 17:15:28 -0400

    Added if statement to check for style class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
-button.addEventListener("click", function(){
-  // console.log("It's working")
-  cat.classList.add("show");
+// button.addEventListener("click", function(){
+//   // console.log("It's working")
+//   cat.classList.add("show");
+//   button.innerText = "Isn't he cute!?"
+// });
+
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
 });
